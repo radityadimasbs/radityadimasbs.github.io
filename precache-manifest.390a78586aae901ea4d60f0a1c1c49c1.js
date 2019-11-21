974a0fb540882ca38a4b1de6d7bc8a7ce9e7f99b
radityadimasbs <radityadimas43@gmail.com>
2019-11-21 12:40:28 +0700

    UGD13_C_9234

---
+++ precache-manifest.390a78586aae901ea4d60f0a1c1c49c1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "618a8cd25d73b9709b73",
+    "url": "/css/about.3b3c9ddb.css"
+  },
+  {
+    "revision": "c18159a60592a14637fb",
+    "url": "/css/chunk-vendors.ec4de8ca.css"
+  },
+  {
+    "revision": "619ecf83ea45c08829673873f0127b1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "618a8cd25d73b9709b73",
+    "url": "/js/about.bb1062b0.js"
+  },
+  {
+    "revision": "c4ac1b7a33ae97b2c77c",
+    "url": "/js/app.a0fd95d0.js"
+  },
+  {
+    "revision": "c18159a60592a14637fb",
+    "url": "/js/chunk-vendors.d1714946.js"
+  },
+  {
+    "revision": "70edebaa2ecd49ae1a1bd411f07a30d7",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
